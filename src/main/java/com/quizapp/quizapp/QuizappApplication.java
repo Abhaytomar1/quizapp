55e760602e496ebcd6777423ab3d6085976e242e
Abhay Tomar <abhaytomar2024@gmail.com>
2024-02-29 11:24:27 +0530

    swagger ui implementation for apis documentation

--- src/main/java/com/quizapp/quizapp/QuizappApplication.java
+++ src/main/java/com/quizapp/quizapp/QuizappApplication.java
@@ -2,6 +2,7 @@ package com.quizapp.quizapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class QuizappApplication {
